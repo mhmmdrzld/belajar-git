a1aab71c693a3d37317b0ff62343f800831d4ab4
Muhammad Rizaldi <mrizaldi1998@gmail.com>
2023-02-12 20:58:48 +0800

    ssss

--- contact.php
+++ contact.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>ini feature contact</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus corporis qui vitae, unde ut cum animi eius expedita! Labore esse laboriosam eligendi adipisci id nesciunt, optio sequi minus ut quaerat.</p>
 </body>
 
 </html>
